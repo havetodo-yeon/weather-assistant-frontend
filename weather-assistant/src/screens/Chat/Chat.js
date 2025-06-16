77b5f6edac2727b26f78db137a68021edec414fe
SoonIL Hong <hong01si@naver.com>
2025-06-16 19:32:02 +0900

    누락된 로컬 주소를 서버주소로 변경
    Chat.js
    http://localhost:4000 -> https://weather-assistant-backend1.onrender.com

--- weather-assistant/src/screens/Chat/Chat.js
+++ weather-assistant/src/screens/Chat/Chat.js
@@ -28,7 +28,7 @@ const Chat = ({
       
       const generateTitle = async () => {
         try {
-          const response = await fetch('http://localhost:4000/generate-title', {
+          const response = await fetch('https://weather-assistant-backend1.onrender.com/generate-title', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ userInput: messages[0].text })
